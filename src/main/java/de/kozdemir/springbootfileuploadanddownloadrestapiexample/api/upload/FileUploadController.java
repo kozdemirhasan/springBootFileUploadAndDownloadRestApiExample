e044232b146757ba0592c5ecb26f0c496fc92900
Hasan Murat Kücüközdemir <kozdemirhasan@gmail.com>
2023-05-15 23:25:01 +0200

    Initial commit

---
+++ src/main/java/de/kozdemir/springbootfileuploadanddownloadrestapiexample/api/upload/FileUploadController.java
@@ -0,0 +1,9 @@
+
+package de.kozdemir.springbootfileuploadanddownloadrestapiexample.api.upload;
+/**
+ * Created By Hasan-Murat Kücüközdemir
+ * Date : 15.05.2023
+ */  
+ 
+public class FileUploadController {
+}
